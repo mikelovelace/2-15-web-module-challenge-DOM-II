969e7885e940022766bb59dce679a0772bb870cc
mikelovelace <miketlovelace@gmail.com>
2022-12-10 22:07:17 -0500

    start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import './less/index.less'
 
-// Your code goes here!
+// links container
+const navContainer = document.querySelector("nav")
+navContainer.addEventListener("dblclick", () => {
+    navContainer.style.background = "gray"
+})
+
+// links
+const navLinks = document.querySelectorAll(".nav-link");
+navLinks.forEach(link => {
+  link.addEventListener("mouseover", () => {
+    link.style.backgroundColor = "orange";
+      //Prevent Default
+  link.addEventListener("click", e => {
+    e.preventDefault();
+  });
+})
+  });
+
+
+  // logo
+  const logoHeading = document.querySelector(".logo-heading")
+  window.addEventListener("load", () => {
+    logoHeading.style.color = "orange"
+    logoHeading.textContent = "I hate Fun Bus"
+  })
+
+  // hero
+  const hero = document.querySelector(".intro img")
+  hero.addEventListener("mouseenter", () => {
+    hero.style.filter = "grayscale(100%)"
+  })
